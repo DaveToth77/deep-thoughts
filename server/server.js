0b4574220e7f2ef2d41ff151bf4d2d0d13646164
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-08 09:10:59 -0500

    added client server, merged server and client

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { authMiddleware} = require('./utils/auth')
+const path = require("path");
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
@@ -33,6 +34,17 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 
 db.once("open", () => {
   app.listen(PORT, () => {
