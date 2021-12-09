a44211e0d3decc4e8164ed8d06356a9b739557d9
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-09 18:49:15 -0500

    updated port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  process.env.MONGODB_URI || "mongodb://localhost:3001/deep-thoughts",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
